ebbb696750ab8fd55a699a940bdedc7f43a3a17a
Akhila Ra <Akhila.Ra@audreeinfotech.com>
2019-06-24 15:41:44 +0530

    Add project files.

---
+++ Services/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Services.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
